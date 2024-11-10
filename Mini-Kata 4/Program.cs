963b44410d15315bd8e17deecd3bd0b0b0a922bb
Munterz <munterskevin@hotmail.com>
2024-11-10 19:09:41 +0100

    Mini-Kata 4

---
+++ Mini-Kata 4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Spawning enemies using a for loop: ");
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Enemy #{i} spawned!");
+}
+
+Console.WriteLine("\nSpawning enemies using a while loop: ");
+int enemiesSpawned = 1;
+int maxEnemies = 3;
+
+while (enemiesSpawned <= maxEnemies)
+
+{
+    Console.WriteLine($"Enemy #{enemiesSpawned} spawned!");
+    enemiesSpawned++;
+}
