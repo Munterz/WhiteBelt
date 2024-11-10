c76fa76658e6c86e70e6e8ef7a6378850bea0293
Munterz <munterskevin@hotmail.com>
2024-11-10 16:16:49 +0100

    Mini-Kata 1 - Variable Practice

---
+++ 2Mini-Kata-1/Program.cs
@@ -0,0 +1,16 @@
+﻿string character = "Neo";
+
+int playerHealth = 100;
+
+float attackPower = 25.5f;
+
+bool isParalyzed = false;
+
+double silverAmount = 72.3;
+
+Console.WriteLine("Characters name: " + character);
+Console.WriteLine("Player's health: " + playerHealth);
+Console.WriteLine("Attack power: " + attackPower);
+Console.WriteLine("Is Paralyzed: " + isParalyzed);
+Console.WriteLine("Silver: " + silverAmount);
+
